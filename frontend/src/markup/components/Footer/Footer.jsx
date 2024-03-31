a803ffe28c45c54da5ecdc1782b3bb53b6970cc1
eliyas41 <fentaeliyas@gmail.com>
2024-03-31 15:20:08 +0300

    Footer Component

--- frontend/src/markup/components/Footer/Footer.jsx
+++ frontend/src/markup/components/Footer/Footer.jsx
@@ -2,7 +2,143 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer class="main-footer">
+      {/* <!--Upper Box--> */}
+      <div class="upper-box">
+        <div class="auto-container">
+          <div class="row no-gutters">
+
+            {/* <!--Footer Info Box--> */}
+            <div class="footer-info-box col-md-4 col-sm-6 col-xs-12">
+              <div class="info-inner">
+                <div class="content">
+                  <div class="icon">
+                    <span class="flaticon-pin"></span>
+                  </div>
+                  <div class="text">54B, Tailstoi Town 5238 MT, <br /> La city, IA 522364</div>
+                </div>
+              </div>
+            </div>
+
+            {/* <!--Footer Info Box--> */}
+            <div class="footer-info-box col-md-4 col-sm-6 col-xs-12">
+              <div class="info-inner">
+                <div class="content">
+                  <div class="icon">
+                    <span class="flaticon-email"></span>
+                  </div>
+                  <div class="text">Email us : <br /> <a
+                    href="mailto:contact.contact@autorex.com">contact@autorex.com</a></div>
+                </div>
+              </div>
+            </div>
+
+            {/* <!--Footer Info Box--> */}
+            <div class="footer-info-box col-md-4 col-sm-6 col-xs-12">
+              <div class="info-inner">
+                <div class="content">
+                  <div class="icon">
+                    <span class="flaticon-phone"></span>
+                  </div>
+                  <div class="text">Call us on : <br /><strong>+ 1800 456 7890</strong></div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      {/* <!--Widgets Section--> */}
+      <div class="widgets-section">
+        <div class="auto-container">
+          <div class="widgets-inner-container">
+            <div class="row clearfix">
+
+              {/* <!--Footer Column--> */}
+              <div class="footer-column col-lg-4">
+                <div class="widget widget_about">
+                  <div class="logo">
+                    <a href="index.html"><img src="assets/images/logo-two.png" alt="" /></a>
+                  </div>
+                  <div class="text">Capitalize on low hanging fruit to identify a ballpark value added
+                    activity to beta test. Override the digital divide additional clickthroughs.
+                  </div>
+                </div>
+              </div>
+
+              {/* <!--Footer Column--> */}
+              <div class="footer-column col-lg-4">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="widget widget_links">
+                      <h4 class="widget_title">Usefull Links</h4>
+                      <div class="widget-content">
+                        <ul class="list">
+                          <li><a href="index.html">Home</a></li>
+                          <li><a href="about.html">About Us</a></li>
+                          <li><a href="#">Appointment</a></li>
+                          <li><a href="testimonial.html">Testimonials</a></li>
+                          <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="widget widget_links">
+                      <h4 class="widget_title">Our Services</h4>
+                      <div class="widget-content">
+                        <ul class="list">
+                          <li><a href="#">Performance Upgrade</a></li>
+                          <li><a href="#">Transmission Service</a></li>
+                          <li><a href="#">Break Repair & Service</a></li>
+                          <li><a href="#">Engine Service & Repair</a></li>
+                          <li><a href="#">Trye & Wheels</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* <!--Footer Column--> */}
+              <div class="footer-column col-lg-4">
+                <div class="widget widget_newsletter">
+                  <h4 class="widget_title">Newsletter</h4>
+                  <div class="text">Get latest updates and offers.</div>
+                  <div class="newsletter-form">
+                    <form class="ajax-sub-form" method="post">
+                      <div class="form-group">
+                        <input type="email" placeholder="Enter your email"
+                          id="subscription-email" />
+                        <button class="theme-btn" type="submit"><span
+                          class="fas fa-paper-plane"></span></button>
+                        <label class="subscription-label" for="subscription-email"></label>
+                      </div>
+                    </form>
+                  </div>
+                  <ul class="social-links">
+                    <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+                    <li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                    <li><a href="#"><span class="fab fa-google-plus-g"></span></a></li>
+                  </ul>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <!--Footer Bottom--> */}
+      <div class="auto-container">
+        <div class="footer-bottom">
+          <div class="copyright-text">© Copyright <a href="#">Autorex</a> 2020 . All right reserved.</div>
+          <div class="text">Created by <a href="#">ThemeArc</a></div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
