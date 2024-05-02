ab1f9b5c64b537e7eeda22bf7a019fcc5b33affb
eliyas41 <fentaeliyas@gmail.com>
2024-05-02 06:29:44 +0300

    add paragraph

--- frontend/src/markup/pages/admin/Customers.jsx
+++ frontend/src/markup/pages/admin/Customers.jsx
@@ -4,6 +4,7 @@ const Customers = () => {
   return (
     <div>
       <h1>Customers Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam dolorem repellendus rem, vero quis, voluptatum libero obcaecati possimus voluptatem delectus architecto inventore voluptatibus recusandae fuga iure odio commodi, nostrum eos.</p>
     </div>
   );
 }
