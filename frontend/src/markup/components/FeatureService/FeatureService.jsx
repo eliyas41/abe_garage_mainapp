972ea222457603675e61a13b27824287f643dc2e
eliyas41 <fentaeliyas@gmail.com>
2024-05-08 14:10:41 +0300

    Finish FeatureService Component

---
+++ frontend/src/markup/components/FeatureService/FeatureService.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const FeatureService = () => {
+  return (
+    // Services Section 
+    <section class="services-section">
+      <div class="auto-container">
+        <div class="sec-title style-two">
+          <h2>Our Featured Services</h2>
+          <div class="text">Bring to the table win-win survival strategies to ensure proactive domination. At
+            the end of the day, going forward, a new normal that has evolved from generation X is on the
+            runway heading towards a streamlined cloud solution. </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Performance Upgrade</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-power"></span></div>
+            </div>
+          </div>
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Transmission Services</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-gearbox"></span></div>
+            </div>
+          </div>
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Break Repair & Service</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-brake-disc"></span></div>
+            </div>
+          </div>
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Engine Service & Repair</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-car-engine"></span></div>
+            </div>
+          </div>
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Tyre & Wheels</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-tire"></span></div>
+            </div>
+          </div>
+          <div class="col-lg-4 service-block-one">
+            <div class="inner-box hvr-float-shadow">
+              <h5>Service and Repairs</h5>
+              <h2>Denting & Painting</h2>
+              <a href="#" class="read-more">read more +</a>
+              <div class="icon"><span class="flaticon-spray-gun"></span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  )
+}
+
+export default FeatureService
--- frontend/src/markup/pages/Home.jsx
+++ frontend/src/markup/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BottomBanner from '../components/BottomBanner/BottomBanner'
 import AboutUs from '../components/AboutUs/AboutUs'
+import FeatureService from '../components/FeatureService/FeatureService'
 
 const Home = () => {
   return (
     <>
       <BottomBanner />
       <AboutUs />
+      <FeatureService />
     </>
   )
 }
